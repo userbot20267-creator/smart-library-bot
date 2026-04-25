7eec28ae602eaa269a77aadf020da5ecd2ff1ad9
userbot20267-creator <userbot20267@gmail.com>
2026-04-25 23:31:21 +0300

    Create __init__.py

---
+++ bot/features/notifications/__init__.py
@@ -0,0 +1,14 @@
+from .notifications_handler import (
+    notification_settings, toggle_notification,
+    manage_preferred_categories, toggle_preferred_category,
+    manage_preferred_authors, toggle_preferred_author,
+    notify_users_new_book
+)
+
+__all__ = [
+    "notification_settings", "toggle_notification",
+    "manage_preferred_categories", "toggle_preferred_category",
+    "manage_preferred_authors", "toggle_preferred_author",
+    "notify_users_new_book"
+]
+
